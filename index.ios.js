77d8fbe58a30cd3cac144cfe6176569c9a16aea2
葉怡伶 <yilinyeh0102@gmail.com>
2017-02-16 11:51:33 +0800

    Update index.ios.js

---
+++ index.ios.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import dva from 'dva/mobile';
+
+import Initial from './src/models/Initial';
+import Auth from './src/models/Auth';
+import Map from './src/models/Map';
+import Message from './src/models/Message';
+
+import Router from './src/Router';
+
+const app = dva();
+
+app.model(Initial);
+app.model(Auth);
+app.model(Map);
+app.model(Message);
+
+app.router(() => <Router />);
+
+AppRegistry.registerComponent('talk_map', () => app.start());
